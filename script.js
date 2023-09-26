457f7433edcc57054d5048ed849e67c1f7e2d286
sami <samiap914@gmail.com>
2023-09-26 21:58:16 +0530

    added the edit feature

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const inbox= document.getElementById("inbox"); 
 const list= document.getElementById("list");
 
+
+
 inbox.focus();
 function addTask(){ 
     if(inbox.value===''){
@@ -10,6 +12,11 @@ function addTask(){
         let task =document.createElement("li");
         task.innerHTML=inbox.value;
         list.appendChild(task);
+
+       let img=document.createElement("img");
+        img.src="images/icons8-edit-50.png";
+        task.appendChild(img);
+   
         let span=document.createElement("span");
         span.innerHTML= "\u00d7"; //the into sign for deletion of tasks
         task.appendChild(span);
@@ -29,11 +36,55 @@ list.addEventListener("click",function(e){
         e.target.classList.toggle("checked");
       saveData();
     }
-    else if(e.target.tagName ==="SPAN"){
-        e.target.parentElement.remove();
-       saveData();
+      else if(e.target.tagName ==="SPAN"){
+      e.target.parentElement.remove();
+      saveData();
+    }
+    else  if(e.target.tagName==="IMG"){
+        editTask(e.target.parentElement);
+        saveData();
     }
 }, false);
+
+
+function editTask(taskElement) {
+   
+    const xSpan = taskElement.querySelector("span");
+    if (xSpan) {
+      xSpan.remove();
+    }
+
+    const input = document.createElement("input");
+    const edittext = taskElement.textContent;
+   
+    input.type = "text";
+    input.value = edittext;
+    
+    input.focus();
+   
+  
+    input.addEventListener("keyup", (event) => {
+      if (event.key === "Enter") {
+       
+
+     taskElement.textContent = input.value;
+
+     let img=document.createElement("img");
+        img.src="images/icons8-edit-50.png";
+        taskElement.appendChild(img);
+   
+        let span=document.createElement("span");
+        span.innerHTML= "\u00d7"; //the into sign for deletion of tasks
+        taskElement.appendChild(span);
+        saveData();
+      }
+    });
+  
+    taskElement.innerHTML = "";
+    taskElement.appendChild(input);
+  }
+
+
 
 function saveData(){
     localStorage.setItem("data",list.innerHTML);
