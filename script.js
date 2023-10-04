c94089be7722050090214917e3caef1c52aa9ca1
sami <samiap914@gmail.com>
2023-10-04 17:40:30 +0530

    Variable definitions moved to the start of scopes, fixed indentation, removed comments,logs

--- script.js
+++ script.js
@@ -1,143 +1,138 @@
-const inbox = document.getElementById("inbox");
-const list = document.getElementById("list");
-let previousTaskElement = "";
+const inbox = document.getElementById('inbox')
+const list = document.getElementById('list')
+let previousTaskElement = ''
 
-inbox.focus();
-function addTask() {
-  if (inbox.value === "") {
-    alert("Invalid entry. Try again.");
+inbox.focus()
+
+function addTask () {
+  const task = document.createElement('li')
+  const img = document.createElement('img')
+  const span = document.createElement('span')
+
+  if (inbox.value === '') {
+    alert('Invalid entry. Try again.')
   } else {
-    const task = document.createElement("li");
-    task.innerHTML = inbox.value;
-    list.appendChild(task);
+    task.innerHTML = inbox.value
+    list.appendChild(task)
 
-    const img = document.createElement("img");
-    img.src = "images/icons8-edit-50.png";
-    task.appendChild(img);
+    img.src = 'images/icons8-edit-50.png'
+    task.appendChild(img)
 
-    const span = document.createElement("span");
-    span.innerHTML = "\u00d7"; //the into sign for deletion of tasks
-    task.appendChild(span);
+    span.innerHTML = '\u00d7'
+    task.appendChild(span)
   }
-  inbox.value = null;
-  saveData();
+  inbox.value = null
+  saveData()
 }
 
-inbox.addEventListener("keyup", function (event) {
-  if (event.key === "Enter") {
-    addTask();
+inbox.addEventListener('keyup', function (event) {
+  if (event.key === 'Enter') {
+    addTask()
   }
-});
+})
 
-list.addEventListener(
-  "click",
-  function (e) {
-    if (e.target.tagName === "LI") {
-      e.target.classList.toggle("checked");
-      saveData();
-      inbox.focus();
-    } else if (e.target.tagName === "SPAN") {
-      e.target.parentElement.remove();
-      saveData();
-      inbox.focus();
-    } else if (e.target.tagName === "IMG") {
-      editTask(e.target.parentElement);
-      saveData();
-    }
-  },
-  false
-);
+addEventListener('click', function (e) {
+  if (e.target.tagName === 'LI') {
+    e.target.classList.toggle('checked')
+    saveData()
+    inbox.focus()
+  } else if (e.target.tagName === 'SPAN') {
+    e.target.parentElement.remove()
+    saveData()
+    inbox.focus()
+  } else if (e.target.tagName === 'IMG') {
+    editTask(e.target.parentElement)
+    saveData()
+  }
+}, false)
 
-function editTask(taskElement) {
-  if (previousTaskElement === "") {
-    //first time editing a task
-    previousTaskElement = taskElement;
+function editTask (taskElement) {
+  const img = document.createElement('img')
+  const span = document.createElement('span')
+  const xSpan = taskElement.querySelector('span')
+  const xImg = taskElement.querySelector('img')
+  const input = document.createElement('input')
+
+  if (previousTaskElement === '') {
+    previousTaskElement = taskElement
   } else {
-    //already in the middle of editing a  task
-    const xInput = previousTaskElement.querySelector("input");
+    const xInput = previousTaskElement.querySelector('input')
+    const text = xInput.value
+    const img = document.createElement('img')
+    const span = document.createElement('span')
 
-    let text = xInput.value;
     if (xInput) {
-      xInput.remove();
+      xInput.remove()
     }
-    console.log("hello");
-    previousTaskElement.textContent = text;
+    previousTaskElement.textContent = text
 
-    const img = document.createElement("img");
-    img.src = "images/icons8-edit-50.png";
-    previousTaskElement.appendChild(img);
+    img.src = 'images/icons8-edit-50.png'
+    previousTaskElement.appendChild(img)
 
-    const span = document.createElement("span");
-    span.innerHTML = "\u00d7";
-    previousTaskElement.appendChild(span);
+    span.innerHTML = '\u00d7'
+    previousTaskElement.appendChild(span)
 
-    previousTaskElement = taskElement;
+    previousTaskElement = taskElement
   }
 
-  const xSpan = taskElement.querySelector("span");
   if (xSpan) {
-    xSpan.remove();
+    xSpan.remove()
   }
 
-  const xImg = taskElement.querySelector("img");
   if (xImg) {
-    xImg.remove();
+    xImg.remove()
   }
 
-  let newTask = taskElement.textContent;
+  const newTask = taskElement.textContent
 
-  const input = document.createElement("input");
-  input.type = "text";
-  input.id = "task";
-  input.value = newTask;
-  taskElement.innerHTML = "";
-  taskElement.appendChild(input);
-  const task = document.getElementById("task");
-  task.focus();
+  input.type = 'text'
+  input.id = 'task'
+  input.value = newTask
+  taskElement.innerHTML = ''
+  taskElement.appendChild(input)
+  span.innerHTML = '\u00d7'
+  taskElement.appendChild(span)
+  saveData()
 
-  //editing happens here
-  const span = document.createElement("span");
-  span.innerHTML = "\u00d7";
-  taskElement.appendChild(span);
-  saveData();
+  const task = document.getElementById('task')
+  task.focus()
 
-  input.addEventListener("keyup", (event) => {
-    if (event.key === "Enter") {
-      const xInput = taskElement.querySelector("input");
-      let text = xInput.value;
-      if (xInput.value === "") {
-        alert("Invalid entry. Try again.");
+  input.addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+      const xInput = taskElement.querySelector('input')
+      const text = xInput.value
+
+      if (xInput.value === '') {
+        alert('Invalid entry. Try again.')
       } else {
         if (xInput) {
-          xInput.remove();
+          xInput.remove()
         }
 
-        taskElement.textContent = text;
+        taskElement.textContent = text
 
         if (taskElement === previousTaskElement) {
-          previousTaskElement = "";
-          inbox.focus();
+          previousTaskElement = ''
+          inbox.focus()
         }
 
-        const img = document.createElement("img");
-        img.src = "images/icons8-edit-50.png";
-        taskElement.appendChild(img);
+        img.src = 'images/icons8-edit-50.png'
+        taskElement.appendChild(img)
 
-        const span = document.createElement("span");
-        span.innerHTML = "\u00d7"; //the into sign for deletion of tasks
-        taskElement.appendChild(span);
-        saveData();
+        span.innerHTML = '\u00d7'
+        taskElement.appendChild(span)
+
+        saveData()
       }
     }
-  });
+  })
 }
-function saveData() {
-  localStorage.setItem("data", list.innerHTML);
+function saveData () {
+  localStorage.setItem('data', list.innerHTML)
 }
 
-function showTask() {
-  list.innerHTML = localStorage.getItem("data");
+function showTask () {
+  list.innerHTML = localStorage.getItem('data')
 }
-showTask();
-
-localStorage.clear();
+showTask()
+localStorage.clear()
